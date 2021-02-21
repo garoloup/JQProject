e73fef741052a50477f3d29023242603d0b52861
garoloup <fredwolff@free.fr>
2021-02-21 18:57:51 +0100

    Animate menu

---
+++ script.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    var $mainMenuItems = $("#main-menu ul").children("li"),
+        totalMainMenuItems = $mainMenuItems.length,
+        openedIndex = -1,
+        init = function(){
+            $mainMenuItems.children(".image").click(function(){
+                var newIndex = $(this).parent().index(),
+                    $item = $mainMenuItems.eq(newIndex),
+                    $colorImage = $item.find(".color");
+
+                $colorImage.animate({left:"0px"},250);
+                $item.animate({width:"420px"},250);
+
+                openedIndex = newIndex;
+            });
+        };
+
+
+    init();
+});
+
+
